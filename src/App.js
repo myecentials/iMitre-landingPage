d1d99586f1aab759133511a5f81109ee90300fda
harold468 <haroldosei24@gmail.com>
2024-05-05 12:37:43 +0000

    'first_section'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./components/homepage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route 
+      path='/'
+       exact
+      element={<Homepage/>}
+      />
+
+    <Route 
+      path='*'
+       exact
+      element={<Homepage/>}
+      />
+
+      
+    </Routes>
+
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/homepage.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./navbar";
+import Sponsors from "./sponsors";
+import Litre_Tracker from "./your_litre_tracker";
+
+const Homepage=()=>{
+    return(
+        <div className="lg:w-full bg-homepage_bg">
+
+            <Navbar/>
+            <Litre_Tracker/>
+            <Sponsors/>
+
+        </div>
+    );
+}
+export default Homepage
