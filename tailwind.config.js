d0ba054ef7ad2fa290e6e33ad76c21e73564d533
harold468 <haroldosei24@gmail.com>
2024-05-06 22:02:32 +0000

    feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,8 @@ module.exports = {
         'imitregreen':'#3EBF53',
         'bg_gray':'#E5F4F2',
         'text_color':'#2D2D2D',
-        'sponsor_border':'#D8D8D8'
+        'sponsor_border':'#D8D8D8',
+        'learn_more':'#009379'
       }
     },
   },
