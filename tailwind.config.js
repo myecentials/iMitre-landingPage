5b461316577b54e1f3a1c9bd04fbb9f718322a4c
harold468 <haroldosei24@gmail.com>
2024-05-09 21:54:55 +0000

    testimonial page

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,10 @@ module.exports = {
         'bg_gray':'#E5F4F2',
         'text_color':'#2D2D2D',
         'sponsor_border':'#D8D8D8',
-        'learn_more':'#009379'
+        'learn_more':'#009379',
+        'testimonial_position_name':'#111827',
+        'testimonial_position_title':'#374151',
+        'star_border':'#F8D57E',
       }
     },
   },
