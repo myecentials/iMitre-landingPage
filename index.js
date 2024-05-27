6da473c56b28fb60eaf2376663e947b73ab3d993
harold <haroldosei24@gmail.com>
2024-05-27 22:06:28 +0000

    node js setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = require("express")()
+const cors = require("cors")
+const path = require("path")
+app.use(cors())
+const PORT=4000
+
+app.use(express.static(path.join(__dirname,'build')))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.get('/',((req,res)=>{
+    res.sendFile(path.join(__dirname,'build','index.html'))
+}))
+
+app.listen(PORT,()=>{
+    console.log('Listening on port 80')
+})
